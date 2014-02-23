ab0e7f8ff1757c7d2f91507430ad8abd0722e9a0
Andrei Frumusanu <luxneb@gmail.com>
2014-02-23 17:22:41 +0100

    Add a NamedRunnable class
    Acts as a runnable which renames and appends the current thread with a given identifier name.

---
+++ src/main/java/com/af/synapse/utils/NamedRunnable.java
@@ -0,0 +1,37 @@
+/**
+ * Author: Andrei F.
+ *
+ * This file is part of the "Synapse" software and is licensed under
+ * under the Microsoft Reference Source License (MS-RSL).
+ *
+ * Please see the attached LICENSE.txt for the full license.
+ */
+
+package com.af.synapse.utils;
+
+/**
+ * Created by Andrei on 23/02/14.
+ */
+public class NamedRunnable implements Runnable {
+    private final Runnable runnable;
+
+    public NamedRunnable(Runnable command) {
+        this.runnable = command;
+    }
+
+    public void run() {
+        String originalName = Thread.currentThread().getName();
+
+        try {
+            String name = getName();
+            Thread.currentThread().setName(originalName+ ": " + name);
+            runnable.run();
+        } finally {
+            Thread.currentThread().setName(originalName);
+        }
+    }
+
+    private String getName() {
+        return "Unnamed";
+    }
+}
