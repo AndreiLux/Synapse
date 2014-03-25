0ae727dc49badb01b13afdece9d2453bbbd6783d
Andrei Frumusanu <luxneb@gmail.com>
2014-03-25 22:26:59 +0100

    Add programmatical view generation to Pane

--- src/main/java/com/af/synapse/elements/SPane.java
+++ src/main/java/com/af/synapse/elements/SPane.java
@@ -9,8 +9,14 @@
 
 package com.af.synapse.elements;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.webkit.WebSettings;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.af.synapse.R;
@@ -23,14 +29,43 @@ import net.minidev.json.JSONObject;
  * Created by Andrei on 30/08/13.
  */
 public class SPane extends BaseElement {
+    private static LinearLayout.LayoutParams layoutParams = null;
+    private static Drawable background = null;
+
+    private static int paddingLeft = Integer.MIN_VALUE;
+    private static int paddingBottom = Integer.MIN_VALUE;
 
     public SPane(JSONObject elm, LinearLayout layout) throws ElementFailureException {
         super(elm, layout);
 
+        if (layoutParams == null)
+            layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        if (background == null)
+            background = Utils.mainActivity.getResources().getDrawable(R.drawable.holo_gradient);
+
+        if (paddingLeft == Integer.MIN_VALUE)
+            paddingLeft = (int) (3 * Utils.density + 0.5f);
+
+        if (paddingBottom == Integer.MIN_VALUE)
+            paddingBottom = (int) (1 * Utils.density + 0.5f);
+
         if (elm.containsKey("title")) {
-            TextView v = (TextView) LayoutInflater.from(Utils.mainActivity)
-                    .inflate(R.layout.template_pane_titlebar, this.layout, false);
-            assert v != null;
+            TextView v;
+
+            if (Utils.useInflater) {
+                v = (TextView) LayoutInflater.from(Utils.mainActivity)
+                        .inflate(R.layout.template_pane_titlebar, this.layout, false);
+                assert v != null;
+            } else {
+                v = new TextView(Utils.mainActivity);
+                v.setLayoutParams(layoutParams);
+                v.setTypeface(Typeface.DEFAULT_BOLD);
+                v.setTextColor(Color.WHITE);
+                v.setBackground(background);
+                v.setPadding(paddingLeft, 0, 0, paddingBottom);
+                v.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
+            }
 
             v.setText(Utils.localise(element.get("title")));
             layout.addView(v);
