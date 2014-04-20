2edbb7ed4ffcaf86824f86baee56e9230bfda347
Andrei Frumusanu <luxneb@gmail.com>
2014-04-20 22:21:44 +0200

    Expand Button into a ActionValueNotifierClient class

--- src/main/java/com/af/synapse/elements/SButton.java
+++ src/main/java/com/af/synapse/elements/SButton.java
@@ -16,6 +16,12 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.af.synapse.MainActivity;
+import com.af.synapse.Synapse;
+import com.af.synapse.lib.ActionNotification;
+import com.af.synapse.lib.ActionValueEvent;
+import com.af.synapse.lib.ActionValueNotifierClient;
+import com.af.synapse.lib.ActionValueNotifierHandler;
+import com.af.synapse.lib.ActivityListener;
 import com.af.synapse.utils.ElementFailureException;
 import com.af.synapse.utils.L;
 import com.af.synapse.R;
@@ -23,10 +29,13 @@ import com.af.synapse.utils.Utils;
 
 import net.minidev.json.JSONObject;
 
+import java.util.ArrayDeque;
+
 /**
  * Created by Andrei on 31/08/13.
  */
-public class SButton extends BaseElement implements View.OnClickListener {
+public class SButton extends BaseElement implements View.OnClickListener,
+                                                    ActionValueNotifierClient, ActivityListener {
     private Button button;
     private String command;
 
@@ -38,6 +47,8 @@ public class SButton extends BaseElement implements View.OnClickListener {
             this.command = element.get("action").toString();
         else
             L.w("Button without action detected!");
+
+        ActionValueNotifierHandler.register(this);
     }
 
     @Override
@@ -58,8 +69,94 @@ public class SButton extends BaseElement implements View.OnClickListener {
         try {
             String result = Utils.runCommand(command);
             Toast.makeText(Utils.mainActivity, result, Toast.LENGTH_LONG).show();
+            ActionValueNotifierHandler.propagate(this, ActionValueEvent.APPLY);
         } catch (Exception e) {
             Toast.makeText(Utils.mainActivity, e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
+    /**
+     *  ActionValueNotifierClient methods
+     */
+
+    @Override
+    public String getId() {
+        return command;
+    }
+
+    private ArrayDeque<ActionNotification> queue = new ArrayDeque<ActionNotification>();
+    private boolean jobRunning = false;
+
+    public void handleNotifications() {
+        jobRunning = true;
+        while (queue.size() > 0) {
+            ActionNotification current = queue.removeFirst();
+            switch (current.notification) {
+                case APPLY:
+                    onClick(null);
+                    break;
+            }
+        }
+        jobRunning = false;
+    }
+
+    private Runnable dequeJob = new Runnable() {
+        @Override
+        public void run() {
+            handleNotifications();
+        }
+    };
+
+    @Override
+    public void onNotify(ActionValueNotifierClient source, ActionValueEvent notification) {
+        L.d(notification.toString());
+        queue.add(new ActionNotification(source, notification));
+
+        if (queue.size() == 1 && !jobRunning)
+            Synapse.executor.execute(dequeJob);
+    }
+
+    /**
+     *  ActionValueClient methods
+     */
+
+    @Override
+    public String getLiveValue() { return null; }
+
+    @Override
+    public String getSetValue() { return null; }
+
+    @Override
+    public String getStoredValue() { return null; }
+
+    @Override
+    public void refreshValue() {}
+
+    @Override
+    public void setDefaults() {}
+
+    @Override
+    public void applyValue() {}
+
+    @Override
+    public void cancelValue() {}
+
+    @Override
+    public void onMainStart() {
+        if (!Utils.mainActivity.isChangingConfigurations() && !Utils.appStarted)
+            try {
+                ActionValueNotifierHandler.addNotifiers(this);
+            } catch (Exception e) {
+                Utils.createElementErrorView(new ElementFailureException(this, e));
+            }
+    }
+
+    @Override
+    public void onStart() {}
+
+    @Override
+    public void onResume() {}
+
+    @Override
+    public void onPause() {}
 }
